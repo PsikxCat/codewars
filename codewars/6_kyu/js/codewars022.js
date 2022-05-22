a374b1cff9336d85d3e3a3c9955540e985805272
Richard Arevalo <arevalogrichard@gmail.com>
2022-05-21 22:52:55 -0500

    22 Shortest Word

---
+++ codewars/6_kyu/js/codewars022.js
@@ -0,0 +1,13 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+*/
+
+function findShort(s){
+  let short = s.split(' ').sort((a, b) => a.length - b.length);
+  return short[0].length
+}
+
+console.log(findShort('Cuando miras largo tiempo hacia el abismo, el abismo también mira dentro de ti'));
