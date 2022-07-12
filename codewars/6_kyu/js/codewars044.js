b92d803658c70877ff6c64fd75eda71708f7b835
PsikxCat <arevalogrichard@gmail.com>
2022-07-12 11:29:17 -0500

    44 Welcome!

---
+++ codewars/6_kyu/js/codewars044.js
@@ -0,0 +1,69 @@
+/*
+Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+The Task:
+    Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+    Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+
+The Database:
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+
+Possible invalid inputs include:
+
+IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+IP_ADDRESS_NOT_FOUND - ip address not in the database
+IP_ADDRESS_REQUIRED - no ip address was supplied
+*/
+const langs = [
+  {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+];
+
+const greet = (language) => langs[0][language] === undefined ? 'Welcome' : langs[0][language];
+
+console.log(greet('english'));
+console.log(greet('dutch'));
+console.log(greet('spanish'));
+console.log(greet('IP_ADDRESS_INVALID'));
+
+// CodeWars Solution
+
+const greet2 = (language) => langs[0][language] || langs[0].english;
+
+console.log(greet2('english'));
+console.log(greet2('dutch'));
+console.log(greet2('spanish'));
+console.log(greet2('IP_ADDRESS_INVALID'));
